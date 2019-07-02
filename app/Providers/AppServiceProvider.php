21f9617fe6c6434a67d4419eba4d8ac059a4fa2c
tomoyuki.tsuji <tsujit975@gmail.com>
2019-07-02 08:45:57 +0000

    user registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        \Schema::defaultStringLength(191);
 
+    }
     /**
      * Register any application services.
      *
