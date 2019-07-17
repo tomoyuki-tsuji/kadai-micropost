9561571dfb4e5ee6983aa5892389f8165c678611
tomoyuki.tsuji <tsujit975@gmail.com>
2019-07-17 09:31:49 +0000

    timeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    
-    @if(Auth::check())
+    @if (Auth::check())
         <div class="row">
             <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
-                    </div>
-                </div>
+                @include('users.card', ['user' => Auth::user()])
             </aside>
             <div class="col-sm-8">
                 @if (Auth::id() == $user->id)
                     {!! Form::open(['route' => 'microposts.store']) !!}
                         <div class="form-group">
-                            {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'row' => '2']) !!}
-                                {!!Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+                            {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                            {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
                         </div>
                     {!! Form::close() !!}
                 @endif
